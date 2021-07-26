a23a5210860cf5eae02bca71abb0213cc7ff4ceb
BenAlaa <ahmed.alaa0794@gmail.com>
2021-07-26 03:23:21 +0200

    add solutions to SQL Sort Ages challenge

---
+++ Easy/SQLSortAges/solution1.sql
@@ -0,0 +1,1 @@
+SELECT * FROM maintable_6DKIN where LastName = 'Smith' or FirstName = 'Robert' order by age
---
+++ Easy/SQLSortAges/solution2.sql
@@ -0,0 +1,3 @@
+SELECT * FROM maintable_1NOVE
+where LastName = 'Smith' or FirstName = 'Robert'
+order by Age ASC
