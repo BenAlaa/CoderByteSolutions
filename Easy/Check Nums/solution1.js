d7c5ede962d1b94a3d0db56c1dcf6f99d44a6058
BenAlaa <ahmed.alaa0794@gmail.com>
2021-07-26 03:58:29 +0200

    add solutions to Check Nums challenge

---
+++ Easy/Check Nums/solution1.js
@@ -0,0 +1,10 @@
+function CheckNums(num1,num2) { 
+
+  // code goes here
+  return num2 == num1 ? -1 : num2 > num1; 
+            
+}
+   
+// this call is needed to test your function
+// keep this when you submit your code
+CheckNums(num1,num2) 
---
+++ Easy/Check Nums/solution2.js
@@ -0,0 +1,10 @@
+function CheckNums(num1,num2) { 
+
+  // code goes here  
+  return num1==num2 ? "-1": num2>num1?" true":"false"; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+CheckNums(readline());
